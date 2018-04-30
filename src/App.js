47902b961b8413f588c50983766d6a71c339507d
Freddy8 <Contactfreddy11@Gmail.com>
2018-04-29 19:11:44 -0700

    creating rooms

--- src/App.js
+++ src/App.js
@@ -18,10 +18,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Chat</h1>
-        <RoomList
-          firebase={firebase}
-        />
+        <div className="sidenav">
+          <RoomList
+            firebase={firebase}
+          />
+        </div>
       </div>
     );
   }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 class RoomList extends Component {
   constructor(props) {
     super(props);
+    this.createRoom = this.createRoom.bind(this);
+    this.handleChange = this.handleChange.bind(this);
 
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName: ''
     };
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
@@ -16,8 +19,25 @@ class RoomList extends Component {
       const room = snapshot.val();
       room.key = snapshot.key;
       this.setState({ rooms: this.state.rooms.concat( room ) })
-      console.log(room["name"]);
+      console.log(room);
     });
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    if (this.state.newRoomName === ''){
+      alert('Name of New Room cannot be empty');
+    } else {
+      this.roomsRef.push({
+        name: this.state.newRoomName
+      });
+      this.setState({newRoomName: ''})
+      e.target.reset()
+    }
+  }
+
+  handleChange(e) {
+    this.setState({newRoomName: e.target.value});
   }
 
   render() {
@@ -27,6 +47,20 @@ class RoomList extends Component {
           {this.state.rooms.map( (room, index) => (
             <li key={index}> {room.name} </li>
           ))}
+          <form onSubmit={this.createRoom}>
+
+            <input
+              type="text"
+              placeholder="Lair"
+              onChange={this.handleChange}
+             />
+             <br />
+             <input
+               type='submit'
+               value='Create room'
+              />
+          </form>
+
         </ul>
       </div>
     );
